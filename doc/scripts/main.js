a5f7fb1f8235f2bff11a2dd99c958cc406295e29
たすくん <nizi.minato@gmail.com>
2023-02-27 01:53:41 +0900

    first commit

---
+++ doc/scripts/main.js
@@ -0,0 +1,15 @@
+const UpdateMyAge =()=>{
+    const date = new Date();
+
+    const age = date.getFullYear() - 2003 - (date.getMonth() == 11 ? (date.getDate() - 29 < 0) : 1);
+
+    document.querySelectorAll('.my-age').forEach(e=>e.innerText = age);
+
+    requestAnimationFrame(UpdateMyAge);
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    UpdateMyAge();
+
+    setTimeout(()=>document.getElementById('__making-now').style.opacity = 0, 1000);
+});
