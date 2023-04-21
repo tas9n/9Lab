ce4d5614617f7f47fba055337553bdf49435c1f5
tas9n <nizi.minato@gmail.com>
2023-04-21 16:40:09 +0900

    [fix]UpdateMyAge()は読み込み時のみ実行するように

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,16 +1,14 @@
-const UpdateMyAge =()=>{
+const UpdateMyAge = () => {
     const date = new Date();
 
     const age = date.getFullYear() - 2003 - (date.getMonth() == 11 ? (date.getDate() - 29 < 0) : 1);
 
-    document.querySelectorAll('.my-age').forEach(e=>e.innerText = age);
-
-    requestAnimationFrame(UpdateMyAge);
+    document.querySelectorAll('.my-age').forEach(e => e.innerText = age);
 }
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     UpdateMyAge();
 
-    setTimeout(()=>document.getElementById('__making-now').style.opacity = 0, 1000);
-    setTimeout(()=>document.getElementById('__making-now').style.display = 'none', 3500);
+    setTimeout(() => document.getElementById('__making-now').style.opacity = 0, 1000);
+    setTimeout(() => document.getElementById('__making-now').style.display = 'none', 3500);
 });
